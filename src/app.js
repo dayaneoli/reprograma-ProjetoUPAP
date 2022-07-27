aa746e07128bcdacb9e11a41c594ace62bcd2c18
dayaneoli <eng.dayaneoliveira@gmail.com>
2022-07-27 14:16:47 -0300

    Alteracao sem autenticacao

--- src/app.js
+++ src/app.js
@@ -8,24 +8,19 @@ const db = require("./database/mongoConfig");
 
 const app = express();
 
-
 app.use(express.json());
 app.use(cors());
-
 
 const upaRotas = require("./routes/upaRoutes"); 
 const anajacintaRotas = require("./routes/anajacintaRoutes");
 const guanabaraRotas = require("./routes/guanabaraRoutes");
 
-// const usuarioRotas = require("./routes/usuario"); 
 
 //definição das rotas raizes 
 app.use("/upa", upaRotas);
 app.use("/anajacinta", anajacintaRotas);
 app.use("/guanabara", guanabaraRotas);
 
-// app.use("/usuario", usuarioRotas);
 db.connect();
 
-//Exportar a aplicação
 module.exports = app;
